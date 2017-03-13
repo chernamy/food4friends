0eab82c48e409d77fe06cff2dce04543407c9a2e
Mickey Chao <mjchao2@gmail.com>
2017-03-13 09:50:28 -0400

    get backend test cases to work with new static/images dir

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,10 @@
+import os
+
 env = dict(
     host = "0.0.0.0",
     port = 3000,
     state = "test",
     db_user = "root",
     db_passwd = "root",
-    image_dir = "images",
+    image_dir = os.path.join("static", "images"),
 )
