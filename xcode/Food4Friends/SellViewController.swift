4f8fef20838deaeb9e07f6ed5f5c938a9a6b5da1
Vaish Raman <vaishr@umich.edu>
2017-03-13 10:09:27 -0400

    sell view controller

--- xcode/Food4Friends/SellViewController.swift
+++ xcode/Food4Friends/SellViewController.swift
@@ -6,41 +6,66 @@
 //  Copyright © 2017 Paramount. All rights reserved.
 //
 
+//
+//  SellViewController.swift
+//  Food4Friends
+//
+//  Created by Vaish Raman on 2/12/17.
+//  Copyright © 2017 Paramount. All rights reserved.
+//
+
 import UIKit
 
-import FBSDKCoreKit
-import FBSDKLoginKit
-
 class SellViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imageTaken = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let imagePicker = UIImagePickerController()
-        
-        imagePicker.delegate = self
-        imagePicker.sourceType = UIImagePickerControllerSourceType.camera
-        imagePicker.allowsEditing = false
-        
-        
-        self.present(imagePicker, animated: true,
-                     completion: nil)
-        
-        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-            print(image)
-            dismiss(animated: true, completion: nil)
-        }
-        
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            self.dismiss(animated: true, completion: nil)
-        }
+        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if (!imageTaken) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            
+            
+            self.present(imagePicker, animated: true,
+                         completion: nil)
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageTaken = true
+        print(image)
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        let sellDescriptionViewController : SellDescriptionViewController = self.storyboard?.instantiateViewController(withIdentifier: "SellDescription") as! SellDescriptionViewController
+        self.present(sellDescriptionViewController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
-        
+
+
+
